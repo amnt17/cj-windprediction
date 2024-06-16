f0fe27e2cf2bf8f32a992272792caa9b74ea2b4f
Arief Fadhillah R <arief.fadhillah.r@gmail.com>
2024-06-16 19:51:12 +0700

    edit back to 1

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import streamlit as st
 import requests
 import json
 import threading
-import logging
-
-# Inisialisasi logging
-logging.basicConfig(level=logging.DEBUG)
 
 # Inisialisasi aplikasi Flask
 app = Flask(__name__)
@@ -15,12 +11,7 @@ app = Flask(__name__)
 # Fungsi untuk memuat model
 def load_model():
     global DTReg
-    try:
-        DTReg = load('windprediction_DTReg.pkl')
-        logging.info("Model loaded successfully.")
-    except Exception as e:
-        logging.error("Error loading model: %s", e)
-        DTReg = None
+    DTReg = load('windprediction_DTReg.pkl')
 
 # Muat model saat aplikasi dijalankan
 load_model()
@@ -28,10 +19,6 @@ load_model()
 # Endpoint untuk memprediksi
 @app.route('/predict', methods=['POST'])
 def predict():
-    if DTReg is None:
-        logging.error("Model not loaded")
-        return jsonify({'error': 'Model not loaded'}), 500
-
     try:
         # Ambil data dari request POST
         data = request.get_json()
@@ -44,7 +31,6 @@ def predict():
         
         return jsonify(result)
     except Exception as e:
-        logging.error("Prediction error: %s", e)
         return jsonify({'error': str(e)}), 500
 
 # Fungsi untuk menjalankan Flask di thread terpisah
@@ -53,7 +39,6 @@ def run_flask():
 
 # Jalankan Flask di thread terpisah
 threading.Thread(target=run_flask).start()
-
 
 # Streamlit interface
 st.title('Wind Prediction')
@@ -77,19 +62,11 @@ if st.button('Predict'):
     response = requests.post(url, data=json.dumps(data), headers=headers)
     
     # Handle respons dari server Flask
-    try:
-        response.raise_for_status()  # Akan memunculkan error jika status code tidak 200
-        result = response.json()  # Mencoba decode respons JSON
+    if response.status_code == 200:
+        result = response.json()
         if 'ff_x' in result:
             st.success(f"Prediction: {result['ff_x']}")
         else:
-            st.error('Unexpected response structure')
-    except requests.exceptions.HTTPError as http_err:
-        st.error(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.RequestException as req_err:
-        st.error(f"Request error occurred: {req_err}")
-    except json.decoder.JSONDecodeError as json_err:
-        st.error(f"JSON decode error occurred: {json_err}")
-        st.error(f"Response content: {response.content.decode('utf-8')}")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+            st.error('Error in prediction response')
+    else:
+        st.error('Error in prediction')
